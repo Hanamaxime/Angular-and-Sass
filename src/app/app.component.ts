a9ea913b52fe924786e7a8ede624edaa731745f6
Hana <hanamaxime@gmail.com>
2018-05-31 19:51:08 -0400

    lab4

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title : string = 'Hello World!';
+  subtitle : string = 'This is a subtitle Hana Harrington';
+  altText : string = "Angular Sheild";
+}
